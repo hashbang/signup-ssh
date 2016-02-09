31ad7d95d67f3a535e663ee0d6c121e79ab84946
Aagat Adhikari <hi@aagat.com>
2016-02-09 15:21:04 +0545

    store user data in an object use strict & fixed errors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var Server = require('ssh2').Server;
 
 var welcomeScreen = require('./screens/welcome.js');
@@ -7,13 +9,13 @@ var config = { privateKey: require('fs').readFileSync('keys/host_rsa.key'),};
 function noop(v) {}
 
 var ssh = new Server(config, function(client){
-	var stream,
-	name,
-	keys = [];
+	var stream;
+	var user = {};
+	user.keys = [];
 
 	client.on('authentication', function(ctx){
 
-		name = ctx.username;
+		user.name = ctx.username || 'guest';
 
 		if (ctx.method === 'none') {
 			// You need to generate ssh key message
@@ -28,7 +30,7 @@ var ssh = new Server(config, function(client){
 
 		if ( ctx.method === 'publickey') {
 			console.log('Client connecting using pubkey');
-			keys.push(ctx.key.algo + ' ' + ctx.key.data.toString('base64'));
+			user.keys.push(ctx.key.algo + ' ' + ctx.key.data.toString('base64'));
 			ctx.reject();
 		}
 
@@ -40,7 +42,7 @@ var ssh = new Server(config, function(client){
 
 	client.on('ready', function(ctx){
 		var rows,
-		columns,
+		cols,
 		term;
 
 		client.once('session', function(accept, reject){
@@ -69,7 +71,7 @@ var ssh = new Server(config, function(client){
 				stream = accept();
 
 				// https://github.com/mscdex/ssh2/#pseudo-tty-settings
-				stream.name = name;
+				stream.name = user.name;
 				stream.rows = rows || 24;
 				stream.columns = cols || 80;
 				stream.isTTY = true;
